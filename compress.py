bc0abd19330117e5c7fd580ed81ec203a9aff32d
isaacveg <57183571+isaacveg@users.noreply.github.com>
2023-06-20 20:27:06 +0800

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,5 +1,28 @@
 import os
-import mnn
+import MNN.expr as F
+import time
 
-def param_compress(action='top_k', model):
+def param_compress(model, action='top_k', extra_info=''):
+    """
+    Support multiple compress functions.
+    ## top_k: 
+    select top_k parameters, 
+    'extra_info': the compress ratio how many params remained
+    returns time consumed and params in form of dict.
+
+    ## random
+    randomly select parameters, 
+    'extra_info': the compress ratio how many params remained
+    returns time consumed and params in form of dict.
+
+    ## to_int8
+    Naively turn parameters from float to int8. 
+    'extra_info': unsaturated or saturated
+    returns time consumed and params in form of dict
+    ATTENTION: use 'decompress' to turn it back to fp32 before trainning!
+    """
+    t_0 = time.time()
+    if action == 'top_k':
+        
+        
     
